4eb0fc82bf479367547e65b0c7e6bec9ec12860b
Angela Vegega <angela.vegega@gmail.com>
2022-07-05 22:41:37 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const PORT = 4000
+
+const grammar = {
+  'dake': {
+    japanese: 'だけ',
+    meaning: '限定を表す',
+    exampleSentence: '毎日20分あるくだけの軽い運動をしている。'
+  },
+  'bakari': {
+    japanese: 'ばかり',
+    meaning: '同じもの、同じことが多い、マイナスイメージ',
+    exampleSentence: 'この頃私は失敗ばかりしている。'
+  },
+  'sae': {
+    japanese: 'さえ',
+    meaning: '必要　十分　条件　（さえ～ば/なら）',
+    exampleSentence: '年をとっても、体さえ丈夫なら心配はいらない。'
+  },
+  'shika': {
+    japanese: 'しか',
+    meaning: 'ほかにない、と強調する　（しか～ない）',
+    exampleSentence: 'だれも手伝ってくれない。一人でがんばるしかない。'
+  },
+  'none': {
+    japanese: 'none',
+    meaning: 'none',
+    exampleSentence: 'make an entry yourself'
+  }
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api', (req, res) => {
+  res.json(grammar)
+})
+
+app.get('/api/:lookedUpGrammar', (req, res) => {
+  const reqGrammar = req.params.lookedUpGrammar
+  if(grammar[reqGrammar]) {
+    res.json(grammar[reqGrammar])
+  } else {
+    res.json(grammar['none'])
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`catch your server at port ${PORT}`);
+})
